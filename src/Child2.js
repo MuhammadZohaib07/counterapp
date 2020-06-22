f3c34acdfbb73e2677a63bf3865b80cfcc0354e3
MuhammadZohaib07 <66253658+MuhammadZohaib07@users.noreply.github.com>
2020-06-22 13:38:39 +0500

    updated_dispatch

--- src/Child2.js
+++ src/Child2.js
@@ -1,16 +1,16 @@
 import React, { useReducer } from 'react';
-import CounterReducer from './CounterReducer';
+import counterReducer from './CounterReducer';
 
 const Child2 = () => {
 
-    let [state, dispatch] =  useReducer(CounterReducer, 1);
+    let [state, dispatch] =  useReducer(counterReducer, 1);
 
     return(
         <div>
             <h2>This is second child using Counter Reducer</h2>
 
             <h3>Value of reducer state is: {state}</h3>
-            <button onClick={()=>dispatch('INCREMENT')}>Increment above</button>
+            <button onClick={()=>dispatch({type:'INCREMENT'})}>Increment above</button>
         </div>
     )
 }
